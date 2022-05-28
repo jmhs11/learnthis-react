c7edb5308ea74e8e2e8d06c9ecc39528348ce30e
jmhs11 <josemariahs@gmail.com>
2022-05-28 22:45:15 +0200

    Eventos y formularios section

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,14 +1,31 @@
+import { useState } from 'react';
 import UserRow from './UserRow';
 import style from './UsersList.module.css';
 
 const UsersList = ({ users, children }) => {
-	const usersRendered = users.map(user => (
-		<UserRow key={user.name} {...user} />
-	));
+	const [search, setSearch] = useState('J');
+
+	const usersFiltered = search
+		? users.filter(user =>
+				user.name.toLowerCase().startsWith(search.toLowerCase())
+		  )
+		: users;
+
+	const usersRendered = usersFiltered.length ? (
+		usersFiltered.map(user => <UserRow key={user.name} {...user} />)
+	) : (
+		<p>No hay usuarios</p>
+	);
 
 	return (
 		<div className={style.list}>
 			{children}
+			<input
+				type='text'
+				name='search'
+				value={search}
+				onChange={ev => setSearch(ev.target.value)}
+			/>
 			{usersRendered}
 		</div>
 	);
