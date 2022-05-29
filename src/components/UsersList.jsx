c63744fa3854ae1ea6f0573b820d876cbd24c67c
jmhs11 <josemariahs@gmail.com>
2022-05-29 15:35:16 +0200

    Refactor y funciones puras

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -4,32 +4,72 @@ import style from './UsersList.module.css';
 
 const UsersList = ({ users }) => {
 	const [search, setSearch] = useState('');
+	const [onlyActive, setOnlyActive] = useState(false);
+	const [sortBy, setSortBy] = useState(0);
 
-	const usersFiltered = filterUsersByName(users, search);
+	let usersFiltered = filterActiveUsers(users, onlyActive);
+	usersFiltered = filterUsersByName(usersFiltered, search);
+	usersFiltered = sortUsers(usersFiltered, sortBy);
 	const usersRendered = renderUsers(usersFiltered);
 
 	return (
 		<div className={style.list}>
 			<h1>Listado de Usuarios</h1>
-			<input
-				type='text'
-				name='search'
-				value={search}
-				onChange={ev => setSearch(ev.target.value)}
-			/>
+			<form className={style.form}>
+				<input
+					type='text'
+					value={search}
+					onChange={ev => setSearch(ev.target.value)}
+				/>
+				<div className={style.active}>
+					<input
+						type='checkbox'
+						checked={onlyActive}
+						onChange={ev => setOnlyActive(ev.target.checked)}
+					/>
+					<span>Sólo activos</span>
+				</div>
+				<select
+					value={sortBy}
+					onChange={ev => setSortBy(Number(ev.target.value))}
+				>
+					<option value={0}>Por defecto</option>
+					<option value={1}>Por nombre</option>
+				</select>
+			</form>
 			{usersRendered}
 		</div>
 	);
 };
 
 const filterUsersByName = (users, search) => {
-	if (!search) return users;
+	if (!search) return [...users];
 
 	const lowerCasedSearch = search.toLowerCase();
 
 	return users.filter(user =>
 		user.name.toLowerCase().startsWith(lowerCasedSearch)
 	);
+};
+
+const filterActiveUsers = (users, active) => {
+	if (!active) return [...users];
+
+	return users.filter(user => user.active);
+};
+
+const sortUsers = (users, sortBy) => {
+	const sortedUsers = [...users];
+	switch (sortBy) {
+		case 1:
+			return sortedUsers.sort((a, b) => {
+				if (a.name > b.name) return 1;
+				if (a.name < b.name) return -1;
+				return 0;
+			});
+		default:
+			return sortedUsers;
+	}
 };
 
 const renderUsers = users => {
